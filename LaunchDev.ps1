fca8f186efcd4ef57b2ddaf664fcb726dbfa273b
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-04-04 11:06:27 +0200

    remove scripts

--- LaunchDev.ps1
+++
@@ -1,4 +0,0 @@
-TypeWriter build --branch=Server
-rm ./src/Main/resources/Server.twr
-cp ./.TypeWriter/Build/Static-Server.twr ./src/Main/resources/Server.twr
-TypeWriter executebuild
--- LaunchDev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-TypeWriter build --branch=Server
-rm ./src/Main/resources/Server.twr
-cp ./.TypeWriter/Build/Static-Server.twr ./src/Main/resources/Server.twr
-TypeWriter executebuild
